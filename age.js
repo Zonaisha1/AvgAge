805d9a590ff3d95d1d6e5aa00733ae7d7307b8f3
Zonaisha Fatima <fatimazonaisha@gmail.com>
2025-04-25 13:27:52 +0500

    Add files via upload

---
+++ age.js
@@ -0,0 +1,25 @@
+let Age1 = parseInt(prompt('Enter age of Student 1'));
+let Age2 = parseInt(prompt('Enter age of Student 2'));
+let Age3 = parseInt(prompt('Enter age of Student 3'));
+let Age4 = parseInt(prompt('Enter age of Student 4'));
+let Age5 = parseInt(prompt('Enter age of Student 5'));
+let Age6 = parseInt(prompt('Enter age of Student 6'));
+let Age7 = parseInt(prompt('Enter age of Student 7'));
+let Age8 = parseInt(prompt('Enter age of Student 8'));
+let Age9 = parseInt(prompt('Enter age of Student 9'));
+let Age10 = parseInt(prompt('Enter age of Student 10'));
+console.log(Age1);
+console.log(Age2);
+console.log(Age3);
+console.log(Age4);
+console.log(Age5);
+console.log(Age6);
+console.log(Age7);
+console.log(Age8);
+console.log(Age9);
+console.log(Age10);
+let total = Age1 + Age2 + Age3 + Age4 + Age5 + Age6 + Age7 + Age8 + Age9 + Age10;
+let Avg = total / 10;
+console.log("The Average of Age of All students is : "+Avg);
+
+
